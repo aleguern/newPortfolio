e604595fe7a35fde202f085f87f57bc97c7f773c
aleguern <antoineleguern@yahoo.fr>
2019-01-10 12:00:58 +0100

    Initial Commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>About me</h1>
+    <div style={{ height: "1800px" }}>
+      <p>I'm antoine</p>
+    </div>
+    <p>I'm antoine</p>
+  </Layout>
+);
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>I'd lpve to talk</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
+);
